b3560fd71d1fe85e5ad094cac0932e7d30c6cbed
unknown <Administrator@phanhoi-mkt-pcn.dxmb.com>
2023-05-05 08:55:56 +0700

    8:55

--- dist/src/router/userRouter.js
+++ dist/src/router/userRouter.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.userRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const user_controller_1 = __importDefault(require("../controller/user-controller"));
+const auth_1 = require("../middleware/auth");
 exports.userRouter = (0, express_1.default)();
 exports.userRouter.post('/register', user_controller_1.default.register);
 exports.userRouter.post('/login', user_controller_1.default.login);
+exports.userRouter.use(auth_1.auth);
 exports.userRouter.post('/change-password/:id', user_controller_1.default.changePassword);
 //# sourceMappingURL=userRouter.js.map
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,8 +1,10 @@
 import router from "express";
 import userController from "../controller/user-controller";
+import {auth} from "../middleware/auth";
 
 export const userRouter = router();
 
 userRouter.post('/register',userController.register);
 userRouter.post('/login',userController.login);
+userRouter.use(auth);
 userRouter.post('/change-password/:id',userController.changePassword);
